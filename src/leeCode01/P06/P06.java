fd7a53354a512b094aa8017df4d4cae00a398474
ly <137544897@qq.com>
2023-03-22 10:25:29 +0800

    P06

--- src/leeCode01/P06/P06.java
+++ src/leeCode01/P06/P06.java
@@ -40,25 +40,25 @@ import java.util.List;
 public class P06 {
     public static void main(String[] args) {
         // String s = "PAYPALISHIRING";
-        // String s = "ABC";
-        // int numRows = 2;
         String s = "PAYPALISHIRING";
-        int numRows = 5;
+        // int numRows = 2;
+//        String s = "PAYPALISHIRING";
+        int numRows = 3;
         /**
          * A
          * B
          * C E
          * D
-         * 
-         * 
-         * 
-         * 
-         * P        H
-         * A     S  I
-         * Y   I    R
-         * P L      I  G
-         * A        N
-         * 
+         *
+         *
+         *
+         *
+         * P H
+         * A S I
+         * Y I R
+         * P L I G
+         * A N
+         *
          * "PHASIYIRPLIGAN"
          */
         String convertString = convert(s, numRows);
@@ -66,71 +66,110 @@ public class P06 {
     }
 
     public static String convert(String s, int numRows) {
-        if (s.isEmpty()) {
+        if(numRows<2){
             return s;
         }
-        if (numRows <= 1) {
-            return s;
-        }
-        if (s.length() < numRows) {
-            return s;
-        }
-        int len = s.length();
-        // 拆分起来 第一个等于numRows+(numRows-2)
-        /// 每一束数字一共有多少字（包括中间的横栏）
-        int rowsCharNums = numRows + numRows - 2;
-        if (rowsCharNums < 1) {
-            return s;
-        }
-        if (rowsCharNums > len) {
-            rowsCharNums = len;
-        }
-        /// 一共有这么多束数字
-        int rowsCount = len / rowsCharNums;
-        /// 除开后的余数
-        int residue = len % rowsCharNums;
-
-        /// 把每束字丢进去
-        List<String> array = new ArrayList<>();
-        System.out.println("rowsChatNums=" + rowsCharNums + " rowsCount=" + rowsCount
-                + " residue=" + residue);
-        for (int i = 0; i < rowsCount; i++) {
-            String charStr = s.substring(i * (rowsCharNums), rowsCharNums * (i + 1));
-            String fullCharStr = charStr.substring(0, numRows);
-            String residueStr = charStr.substring(numRows);
-            array.add(fullCharStr);
-            for (int j = 0; j < residueStr.length(); j++) {
-                int index = numRows - 1 - (1 + j);
-                char[] arrayChar = new char[numRows];
-                for (int k = 0; k < numRows; k++) {
-                    if (index == k) {
-                        arrayChar[k] = residueStr.charAt(j);
-                    } else {
-                        arrayChar[k] = '-';
-                    }
-                }
-                array.add(String.valueOf(arrayChar));
-
-            }
-        }
-        if (residue > 0) {
-            array.add(s.substring(s.length() - residue) + "-".repeat(Math.max(0, numRows
-                    - residue)));
-        }
-
-        for (String str : array) {
-            System.out.println(str);
-        }
-
-        StringBuilder sb = new StringBuilder();
+        List<StringBuilder> sbList = new ArrayList<>();
         for (int i = 0; i < numRows; i++) {
-            for (String value : array) {
-                if (value.charAt(i) != '-') {
-                    sb.append(value.charAt(i));
-                }
-            }
+            sbList.add(new StringBuilder());
+        }
+        int i = 0;
+        int flag = -1;
+        for (char charStr : s.toCharArray()) {
+            sbList.get(i).append(charStr);
+            if (i == 0 || i == numRows - 1) flag = -flag;
+            i += flag;
         }
 
-        return sb.toString();
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder row : sbList) res.append(row);
+        return res.toString();
     }
+//    public static String convert(String s, int numRows) {
+//        if (s.isEmpty()) {
+//            return s;
+//        }
+//        if (numRows <= 1) {
+//            return s;
+//        }
+//        if (s.length() < numRows) {
+//            return s;
+//        }
+//        int len = s.length();
+//        // 拆分起来 第一个等于numRows+(numRows-2)
+//        /// 每一束数字一共有多少字（包括中间的横栏）
+//        int rowsCharNums = numRows + numRows - 2;
+//        if (rowsCharNums < 1) {
+//            return s;
+//        }
+//        if (rowsCharNums > len) {
+//            rowsCharNums = len;
+//        }
+//        /// 一共有这么多束数字
+//        int rowsCount = len / rowsCharNums;
+//        /// 除开后的余数
+//        int residue = len % rowsCharNums;
+//
+//        /// 把每束字丢进去
+//        List<String> array = new ArrayList<>();
+//        System.out.println("rowsChatNums=" + rowsCharNums + " rowsCount=" + rowsCount
+//                + " residue=" + residue);
+//        for (int i = 0; i < rowsCount; i++) {
+//            String charStr = s.substring(i * (rowsCharNums), rowsCharNums * (i + 1));
+//            String fullCharStr = charStr.substring(0, numRows);
+//            String residueStr = charStr.substring(numRows);
+//            array.add(fullCharStr);
+//            for (int j = 0; j < residueStr.length(); j++) {
+//                int index = numRows - 1 - (1 + j);
+//                char[] arrayChar = new char[numRows];
+//                for (int k = 0; k < numRows; k++) {
+//                    if (index == k) {
+//                        arrayChar[k] = residueStr.charAt(j);
+//                    } else {
+//                        arrayChar[k] = '-';
+//                    }
+//                }
+//                array.add(String.valueOf(arrayChar));
+//
+//            }
+//        }
+//        if (residue > 0) {
+//            if (residue <= numRows) {
+//                array.add(s.substring(s.length() - residue) + "-".repeat(Math.max(0, numRows
+//                        - residue)));
+//            } else {
+//                String residueStr = charStr.substring(numRows);
+//                int otherNum = residue - numRows;
+//                array.add(s.substring(s.length() - numRows));
+//                for (int i = 0; i < otherNum; i++) {
+//                    int index = numRows - 1 - (1 + i);
+//
+//                    char[] arrayChar = new char[numRows];
+//                    for (int k = 0; k < numRows; k++) {
+//                        if (index == k) {
+//                            arrayChar[k] = residueStr.charAt(j);
+//                        } else {
+//                            arrayChar[k] = '-';
+//                        }
+//                    }
+//                    array.add(String.valueOf(arrayChar));
+//                }
+//            }
+//        }
+//
+//        for (String str : array) {
+//            System.out.println(str);
+//        }
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < numRows; i++) {
+//            for (String value : array) {
+//                if (value.charAt(i) != '-') {
+//                    sb.append(value.charAt(i));
+//                }
+//            }
+//        }
+//
+//        return sb.toString();
+//    }
 }
